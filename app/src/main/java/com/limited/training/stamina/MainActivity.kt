e5e6ad0958fd548ecae62d0800377a150d50dd65
manuel.couto1 <manuel.couto1@udc.es>
2022-03-09 13:33:09 +0100

    ver rutas y toolbar

--- app/src/main/java/com/limited/training/stamina/MainActivity.kt
+++ app/src/main/java/com/limited/training/stamina/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.limited.training.stamina
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_routes_watch)
+        var toolbar: Toolbar = findViewById(R.id.tool_bar);
+
+        if (toolbar != null) {
+            toolbar.title = "Rutas"
+            toolbar.setTitleTextColor(Color.WHITE)
+            toolbar.setSubtitleTextColor(Color.WHITE)
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowHomeEnabled(true);
+            toolbar.bringToFront()
+        }
+
     }
+
 }
