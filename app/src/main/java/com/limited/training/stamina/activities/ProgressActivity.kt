1cf1f48a0eea9fb38ed779b9d07e4ea5d49b1761
manuel-couto <manuel.couto@udc.es>
2022-03-12 20:31:05 +0100

    Flujo entre actividades implementado (Sin arrastrar datos)
    No pasa datos entre actividades.
    Falta que la pantalla de regsitrar actividad aparezca por defecto con el teclado activado

--- app/src/main/java/com/limited/training/stamina/activities/ProgressActivity.kt
+++ app/src/main/java/com/limited/training/stamina/activities/ProgressActivity.kt
@@ -1,12 +1,23 @@
 package com.limited.training.stamina.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.limited.training.stamina.R
+
+var stopButton : Button ?= null
 
 class ProgressActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activity_progress)
+
+        stopButton = findViewById(R.id.stop_btn)
+        stopButton!!.setOnClickListener {
+            val intActivityPaused : Intent = Intent(applicationContext, PausedActivity::class.java)
+            startActivity(intActivityPaused)
+        }
     }
 }
--- app/src/main/java/com/limited/training/stamina/activities/StartActivity.kt
+++ app/src/main/java/com/limited/training/stamina/activities/StartActivity.kt
@@ -1,12 +1,27 @@
 package com.limited.training.stamina.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.limited.training.stamina.R
 
 class StartActivity : AppCompatActivity() {
+
+    var startButton : Button ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activity_start)
+
+        startButton = findViewById(R.id.start_btn)
+        startButton!!.setOnClickListener {
+//            Toast.makeText(this, "mensaje", Toast.LENGTH_SHORT).show()
+            val intActivityInProgress : Intent = Intent(applicationContext, ProgressActivity::class.java)
+            startActivity(intActivityInProgress)
+        }
+
+
     }
 }
