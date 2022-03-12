75b15046e81d19287e6ce0cc62968e119407f339
manuel-couto <manuel.couto@udc.es>
2022-03-12 17:19:06 +0100

    Creados ficheros kt y añadidos al Manifest para el control de las activities

---
+++ app/src/main/java/com/limited/training/stamina/activities/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.limited.training.stamina.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.limited.training.stamina.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_activity_start)
+    }
+}
