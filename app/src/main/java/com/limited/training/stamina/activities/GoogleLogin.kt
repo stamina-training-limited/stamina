80d24e5949d8beaa74a224e79901b3f5bde95559
Jose Figueiras <josefigueirasm@protonmail.com>
2022-03-23 13:11:00 +0100

    Métodos onCreate para actividades

--- app/src/main/java/com/limited/training/stamina/activities/GoogleLogin.kt
+++ app/src/main/java/com/limited/training/stamina/activities/GoogleLogin.kt
@@ -1,6 +1,13 @@
 package com.limited.training.stamina.activities
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.limited.training.stamina.R
 
 class GoogleLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_google)
+    }
 }
