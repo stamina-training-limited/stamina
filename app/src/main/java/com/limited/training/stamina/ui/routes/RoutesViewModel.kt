b1638e6ec92f41d97f4bdd2332eae0a0ef31da07
manuel.couto1 <manuel.couto1@udc.es>
2022-03-23 08:41:06 +0100

    all new icons

--- app/src/main/java/com/limited/training/stamina/ui/routes/RoutesViewModel.kt
+++ app/src/main/java/com/limited/training/stamina/ui/routes/RoutesViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class RoutesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is Routes Fragment"
     }
     val text: LiveData<String> = _text
 }
