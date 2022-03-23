cd893bfc571140d15285751cabf3a4cf78f63017
Jose Figueiras <josefigueirasm@protonmail.com>
2022-03-23 14:26:02 +0100

    Navegación de pantallas de login
    Falta integración con login de google

--- app/src/main/java/com/limited/training/stamina/activities/GoogleLogin.kt
+++ app/src/main/java/com/limited/training/stamina/activities/GoogleLogin.kt
@@ -1,7 +1,10 @@
 package com.limited.training.stamina.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.limited.training.stamina.MainActivity
 import com.limited.training.stamina.R
 
 class GoogleLogin : AppCompatActivity() {
@@ -9,5 +12,18 @@ class GoogleLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_google)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Busqueda de botones para transiciones
+        val login : Button = findViewById(R.id.btn_login_google_login)
+
+        // Transicion hacia pantalla de feed
+        login.setOnClickListener {
+            val intLogin : Intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intLogin)
+        }
     }
 }
--- app/src/main/java/com/limited/training/stamina/activities/MainScreen.kt
+++ app/src/main/java/com/limited/training/stamina/activities/MainScreen.kt
@@ -1,6 +1,8 @@
 package com.limited.training.stamina.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.limited.training.stamina.R
 
@@ -9,5 +11,18 @@ class MainScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_screen)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Búsqueda de botones para transiciones
+        val loginGoogleButton : Button = findViewById(R.id.btn_main_screen_login_google)
+
+        // Transición a login de google
+        loginGoogleButton.setOnClickListener {
+            val intLoginGoogle : Intent = Intent(applicationContext, GoogleLogin::class.java)
+            startActivity(intLoginGoogle)
+        }
     }
 }
