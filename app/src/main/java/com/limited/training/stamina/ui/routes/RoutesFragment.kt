c479a6ef421fd42b48c014394d49251577d15540
manuc <manu.couto1k@gmail.com>
2022-04-08 18:35:20 +0200

    rutas con tarjetas

--- app/src/main/java/com/limited/training/stamina/ui/routes/RoutesFragment.kt
+++ app/src/main/java/com/limited/training/stamina/ui/routes/RoutesFragment.kt
@@ -32,7 +32,17 @@ class RoutesFragment : Fragment() {
 
         _binding = FragmentRoutesBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val routes: ArrayList<String> = arrayListOf("Ruta1", "Ruta2", "Ruta3", "Ruta4", "Ruta5", "Ruta6", "Ruta7", "Ruta8", "Ruta9", "Ruta10", "Ruta11")
+        val routes: ArrayList<String> = arrayListOf(
+            "Ruta sierra morena",
+            "Ruta mortal me morí",
+            "Esta era la facilita",
+            "Aquí me caí",
+            "La de la viaja entrañable",
+            "Tramo camino de santiago",
+            "Donde la zona de petting",
+            "Dura de carallo",
+            "Monte a través"
+        )
         val listView: ListView = binding.listView
 
         listView.adapter = RoutesCustomAdapter(routes, requireActivity().applicationContext)
