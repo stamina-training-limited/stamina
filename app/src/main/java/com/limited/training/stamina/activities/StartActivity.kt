a4e70f5eca3855ecd7a1a4d778c5d9fdf165d6df
manuel-couto <manuel.couto@udc.es>
2022-04-17 20:34:50 +0200

    Conectado con Google Maps
    Funcionalidad "RECORD"
    
    Problemas al integrar botón en fragment. No se muestran botones

--- app/src/main/java/com/limited/training/stamina/activities/StartActivity.kt
+++ app/src/main/java/com/limited/training/stamina/activities/StartActivity.kt
@@ -3,6 +3,7 @@ package com.limited.training.stamina.activities
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.limited.training.stamina.R
 
@@ -16,7 +17,6 @@ class StartActivity : AppCompatActivity() {
 
         startButton = findViewById(R.id.recordStart_btn)
         startButton!!.setOnClickListener {
-//            Toast.makeText(this, "mensaje", Toast.LENGTH_SHORT).show()
             val intActivityInProgress : Intent = Intent(applicationContext, ProgressActivity::class.java)
             startActivity(intActivityInProgress)
         }
