c0f430593ea11a5e1956b5ef408cbb2fcaea3c2a
Jose Figueiras <josefigueirasm@protonmail.com>
2022-04-27 22:56:34 +0200

    Localizacon añadida en sección de registro

--- app/src/main/java/com/limited/training/stamina/ui/record/RecordFragment.kt
+++ app/src/main/java/com/limited/training/stamina/ui/record/RecordFragment.kt
@@ -10,50 +10,107 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
 import android.Manifest
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.Navigation
-import androidx.navigation.Navigator
-import com.google.android.material.snackbar.Snackbar
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.Marker
+
 import com.limited.training.stamina.R
-import com.limited.training.stamina.activities.ProgressActivity
 import com.limited.training.stamina.databinding.FragmentRecordBinding
 
-class RecordFragment : Fragment() {
+class RecordFragment : Fragment(), OnMapReadyCallback {
 
-    private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val fic = LatLng(43.33372, -8.40945)
-        googleMap.addMarker(MarkerOptions().position(fic).title("Estás en FIC"))
-        //googleMap.moveCamera(CameraUpdateFactory.newLatLng(fic))
-        googleMap.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(fic, 17f),
-            4000,
-            null
-        )
+    lateinit var mGoogleMap: GoogleMap
+    var mapFrag: SupportMapFragment? = null
+    lateinit var mLocationRequest: LocationRequest
+    var mLastLocation: Location? = null
+    internal var mCurrLocationMarker: Marker? = null
+    internal var mFusedLocationClient: FusedLocationProviderClient? = null
+
+    internal var mLocationCallback: LocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val locationList = locationResult.locations
+            if (locationList.isNotEmpty()) {
+                //The last location in the list is the newest
+                val location = locationList.last()
+                Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude())
+                mLastLocation = location
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker?.remove()
+                }
+
+                //Place current location marker
+                val latLng = LatLng(location.latitude, location.longitude)
+                val markerOptions = MarkerOptions()
+                markerOptions.position(latLng)
+                markerOptions.title("Current Position")
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))
+                mCurrLocationMarker = mGoogleMap.addMarker(markerOptions)
+
+                //move map camera
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 11.0F))
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        mapFrag = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFrag?.getMapAsync(this)
+    }
+
+    override fun onMapReady (googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+        mGoogleMap.mapType = GoogleMap.MAP_TYPE_HYBRID
+
+        var mLocationRequest : LocationRequest = LocationRequest.create()
+        mLocationRequest.interval = 120000 // two minute interval
+        mLocationRequest.fastestInterval = 120000
+        mLocationRequest.priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                //Location Permission already granted
+                mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()!!)
+                mGoogleMap.isMyLocationEnabled = true
+            } else {
+                //Request Location Permission
+                checkLocationPermission()
+            }
+        } else {
+            mFusedLocationClient?.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper()!!)
+            mGoogleMap.isMyLocationEnabled = true
+        }
     }
 
     private var _binding: FragmentRecordBinding? = null
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -68,78 +125,117 @@ class RecordFragment : Fragment() {
         _binding = FragmentRecordBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        checkLocationService(requireContext(), root)
+        //checkLocationService(requireContext(), root)
 
 
         val dashboardViewModel =
             ViewModelProvider(this).get(RecordViewModel::class.java)
 
 
-//        return inflater.inflate(R.layout.fragment_record, container, false)
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(callback)
 
-        val startButon: Button = binding.recordStartBtn
-        startButon!!.setOnClickListener {
-            val intActivityInProgress: Intent = Intent(activity, ProgressActivity::class.java)
-            startActivity(intActivityInProgress)
-        }
-    }
+
+    //override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    //    super.onViewCreated(view, savedInstanceState)
+    //    val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+    //    mapFragment?.getMapAsync(callback)
+//
+    //    val startButon: Button = binding.recordStartBtn
+    //    startButon!!.setOnClickListener {
+    //        val intActivityInProgress: Intent = Intent(activity, ProgressActivity::class.java)
+    //        startActivity(intActivityInProgress)
+    //    }
+    //}
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
 
-    fun checkLocationService(context : Context, root : View){
-        when {
-            ContextCompat.checkSelfPermission(
-                context,
+    private fun checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED -> {
-                // You can use the API that requires the permission.
-            }
-            shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-            // In an educational UI, explain to the user why your app requires this
-            // permission for a specific feature to behave as expected. In this UI,
-            // include a "cancel" or "no thanks" button that allows the user to
-            // continue using your app without granting the permission.
-                activity?.let {
-                    Snackbar.make(
-                        it.findViewById(android.R.id.content),
-                        getString(R.string.No_location_services), Snackbar.LENGTH_LONG).show()
-                };
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                )
+            ) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                AlertDialog.Builder(requireContext())
+                    .setTitle("Location Permission Needed")
+                    .setMessage("This app needs the Location permission, please accept to use location functionality")
+                    .setPositiveButton(
+                        "OK"
+                    ) { _, _ ->
+                        //Prompt the user once explanation has been shown
+                        ActivityCompat.requestPermissions(
+                            requireActivity(),
+                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            MY_PERMISSIONS_REQUEST_LOCATION
+                        )
+                    }
+                    .create()
+                    .show()
 
 
-            }
-            else -> {
-                // You can directly ask for the permission.
-                // The registered ActivityResultCallback gets the result of this request.
-                requestPermissionLauncher.launch(
-                    Manifest.permission.ACCESS_FINE_LOCATION)
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(
+                    requireActivity(),
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    MY_PERMISSIONS_REQUEST_LOCATION
+                )
             }
         }
     }
 
-    val requestPermissionLauncher =
-        registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted: Boolean ->
-            if (isGranted) {
-                // Permission is granted. Continue the action or workflow in your
-                // app.
-            } else {
-                // Explain to the user that the feature is unavailable because the
-                // features requires a permission that the user has denied. At the
-                // same time, respect the user's decision. Don't link to system
-                // settings in an effort to convince the user to change their
-                // decision.
-            }
-        }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>, grantResults: IntArray
+    ) {
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) == PackageManager.PERMISSION_GRANTED
+                    ) {
+
+                        mFusedLocationClient?.requestLocationUpdates(
+                            mLocationRequest,
+                            mLocationCallback,
+                            Looper.myLooper()!!
+                        )
+                        mGoogleMap.setMyLocationEnabled(true)
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(requireContext(), "permission denied", Toast.LENGTH_LONG).show()
+                }
+                return
+            }
+        }// other 'case' lines to check for other
+        // permissions this app might request
+    }
+
+    companion object {
+        val MY_PERMISSIONS_REQUEST_LOCATION = 99
+    }
 }
